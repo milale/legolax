ad858a5de417f40b61887ba3a3ef2c8c2be31dd4
Sergio Infante <sergio@neosergio.net>
2011-11-21 17:06:25 -0500

    Agregando articulos y registro de los mismos al administrador

--- legolaxweb/portal/admin.py
+++ legolaxweb/portal/admin.py
@@ -3,8 +3,12 @@ from portal.models import Equipo
 from portal.models import Interesado
 from portal.models import TipoDocumento
 from portal.models import Documento
+from portal.models import Articulo
+from portal.models import RegistroArticulo
 
 admin.site.register(Equipo)
 admin.site.register(Interesado)
 admin.site.register(TipoDocumento)
 admin.site.register(Documento)
+admin.site.register(Articulo)
+admin.site.register(RegistroArticulo)
