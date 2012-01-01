fb1d6d942925fb97915b60083780f7ee2faa158f
Sergio Infante <sergio@neosergio.net>
2012-01-01 02:19:27 -0500

    Finalizando la migración de los datos de producción y almacen

--- legolaxweb/cargar.py
+++ legolaxweb/cargar.py
@@ -29,7 +29,7 @@ print "Migrando datos de articulos para almacén"
 lector_almacen = csv.reader(open(csv_almacen), delimiter=',', quotechar='"')
 contador_almacen = 0
 for elemento in lector_almacen:
-	if elemento[0] != "artículo":
+	if elemento[0] != "Artículo":
 		articulo = Articulo()
 		articulo.nombre = elemento[0]
 		articulo.marca = elemento[1]
@@ -47,77 +47,82 @@ print "Fin de la migración de datos de articulos para almacén"
 
 #Descomentar estas líneas solamente si se esta seguro de migrar los datos de almacen de entrada y salida
 #si no se tiene cuidado se registraran duplicados
-#~ print "Migrando datos de registros de entradas y salidas en almacén"
-#~ lector_almacen = csv.reader(open(csv_almacen), delimiter=',', quotechar='"')
-#~ contador = 0
-#~ for elemento in lector_almacen:
-	#~ if elemento[0] != "artículo":
-		#~ registro = RegistroArticulo()
-		#~ articulorelacionado = Articulo.objects.get(nombre__exact=elemento[0]) 
-		#~ registro.articulo = articulorelacionado
-		#~ registro.detalle = elemento[7]
-		#~ registro.cantidad = Decimal(str(float(elemento[8])))
-		#~ registro.fregistro = elemento[5]
-		#~ 
-		#~ if elemento[6] == 'entrada':
-			#~ registro.tipo = 'e'
-			#~ articulorelacionado.sactual += registro.cantidad
-		#~ elif elemento[6] == 'salida':
-			#~ registro.tipo = 's'
-			#~ articulorelacionado.sactual -= registro.cantidad
-		#~ articulorelacionado.save()
-		#~ 
-		#~ if elemento[10] == '': elemento[10] = 0
-		#~ registro.preciouni = Decimal(str(float(elemento[10])))
-		#~ if elemento[11] == '': elemento[11] = 0
-		#~ registro.preciototal = Decimal(str(float(elemento[11])))
-		#~ registro.usuario = User.objects.get(pk=2)
-		#~ registro.save()
-		#~ contador +=1 
-		#~ print contador
-#~ print "Fin de migración de datos de entrada y salida"
-
-print "Migrando datos de trabajos de producción"
-lectorDatos = csv.reader(open(csv_trabajos), delimiter=',', quotechar='"')
+print "Migrando datos de registros de entradas y salidas en almacén"
+lector_almacen = csv.reader(open(csv_almacen), delimiter=',', quotechar='"')
 contador = 0
-for elemento in lectorDatos:
-	equipo = Equipo()
-	equipo.nombre = elemento[0]
-	try:
-		equipo.save()
-	except:
-		pass
-	
-	tdocs = TipoDocumento()
-	tdocs.tipo = elemento[3]
-	try:
-		tdocs.save()
-	except:
-		pass
+for elemento in lector_almacen:
+	if elemento[0] != "Artículo":
+		registro = RegistroArticulo()
+		articulorelacionado = Articulo.objects.get(nombre__exact=elemento[0]) 
+		registro.articulo = articulorelacionado
+		registro.detalle = elemento[7]
 		
-	interesados = Interesado()
-	interesados.nombre = elemento[5]
-	try:
-		interesados.save()
-	except:
-		pass
-	
-	documentos = Documento()
-	documentos.tdocumento = TipoDocumento.objects.get(tipo__exact=elemento[3])
-	documentos.codigo = elemento[4]
-	documentos.interesado = Interesado.objects.get(nombre__exact=elemento[5])
-	documentos.equipo = Equipo.objects.get(nombre__exact=elemento[0])
-	documentos.asunto = elemento[6]
-	documentos.tiraje = elemento[7]
-	documentos.fentrega = elemento[2]
-	documentos.contometro = elemento[8]
-	documentos.costo = elemento[9]
-	documentos.nexpediente = elemento[1]
-	try:
-		documentos.save()
-	except:
-		pass
-
-	contador += 1
-	print str(contador)
+		if elemento[8] == '': elemento[8] = 0
+		registro.cantidad = Decimal(str(float(elemento[8])))
+		
+		registro.fregistro = elemento[5]
+		
+		if elemento[6] == 'entrada':
+			registro.tipo = 'e'
+			articulorelacionado.sactual += registro.cantidad
+		elif elemento[6] == 'salida':
+			registro.tipo = 's'
+			articulorelacionado.sactual -= registro.cantidad
+		articulorelacionado.save()
+		
+		if elemento[10] == '': elemento[10] = 0
+		registro.preciouni = Decimal(str(float(elemento[10])))
+		
+		if elemento[11] == '': elemento[11] = 0
+		registro.preciototal = Decimal(str(float(elemento[11])))
+		
+		registro.usuario = User.objects.get(pk=2)
+		registro.save()
+		contador +=1 
+		print contador
 print "Fin de migración de datos de entrada y salida"
+
+#~ print "Migrando datos de trabajos de producción"
+#~ lectorDatos = csv.reader(open(csv_trabajos), delimiter=',', quotechar='"')
+#~ contador = 0
+#~ for elemento in lectorDatos:
+	#~ equipo = Equipo()
+	#~ equipo.nombre = elemento[0]
+	#~ try:
+		#~ equipo.save()
+	#~ except:
+		#~ pass
+	#~ 
+	#~ tdocs = TipoDocumento()
+	#~ tdocs.tipo = elemento[3]
+	#~ try:
+		#~ tdocs.save()
+	#~ except:
+		#~ pass
+		#~ 
+	#~ interesados = Interesado()
+	#~ interesados.nombre = elemento[5]
+	#~ try:
+		#~ interesados.save()
+	#~ except:
+		#~ pass
+	#~ 
+	#~ documentos = Documento()
+	#~ documentos.tdocumento = TipoDocumento.objects.get(tipo__exact=elemento[3])
+	#~ documentos.codigo = elemento[4]
+	#~ documentos.interesado = Interesado.objects.get(nombre__exact=elemento[5])
+	#~ documentos.equipo = Equipo.objects.get(nombre__exact=elemento[0])
+	#~ documentos.asunto = elemento[6]
+	#~ documentos.tiraje = elemento[7]
+	#~ documentos.fentrega = elemento[2]
+	#~ documentos.contometro = elemento[8]
+	#~ documentos.costo = elemento[9]
+	#~ documentos.nexpediente = elemento[1]
+	#~ try:
+		#~ documentos.save()
+	#~ except:
+		#~ pass
+#~ 
+	#~ contador += 1
+	#~ print str(contador)
+#~ print "Fin de migración de datos de entrada y salida"
